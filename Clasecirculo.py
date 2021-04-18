1cd00f1fc7a6495a8d73d85687fc0224593b96be
Luis-Loredo <luis.loredoo@uanl.edu.mx>
2021-04-18 18:03:53 -0500

    Clase circulo

---
+++ Clasecirculo.py
@@ -0,0 +1,23 @@
+radio = ""
+altura = ""
+
+class circulo:
+    def __init__ (Self, radio, altura ):
+        self.radio = radio
+        self.altura = altura
+    
+    def area(self):
+        self.radio ** 2
+
+    def prisma(self):
+        self.area * altura
+    
+    def piramide(self):
+        self.area * altura / 3
+
+radio = input("Dime el radio: ")
+altura = input("Dime la altura: ")
+
+print ("El area es: ", radio )
+print ("El prisma es: ", radio * altura)
+print ("La piramide es: ", radio * altura / 3)
